1cbdceacddfe3600fff1a9b9a07bea0b7a36b016
va1bhav-sh <vaibhavsharma.dev28@gmail.com>
2025-09-18 17:58:13 +0530

    Circuler Linked List

---
+++ Linked List/circualr_linked_list_creation.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node*next;
+};
+struct Node*cllTraveral(struct Node*head){
+    struct Node*temp=head;
+    do
+    {
+        printf("Element=%d\n",temp->data);
+        temp=temp->next;
+    } while (temp!=head);
+   
+}
+
+int main(){
+    struct Node*head=(struct Node*)malloc(sizeof(struct Node));
+    struct Node*second=(struct Node*)malloc(sizeof(struct Node));
+    struct Node*third=(struct Node*)malloc(sizeof(struct Node));
+    struct Node*fourth=(struct Node*)malloc(sizeof(struct Node));
+    head->data=7;
+    head->next=second;
+    second->data=3;
+    second->next=third;
+    third->data=6;
+    third->next=fourth;
+    fourth->data=8;
+    fourth->next=head;
+   cllTraveral(head);
+
+    return 0;
+}
