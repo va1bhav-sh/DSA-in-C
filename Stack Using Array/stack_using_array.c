986c7d634ddce2e13b25e0976d87749126a15ff6
va1bhav-sh <vaibhavsharma.dev28@gmail.com>
2025-09-28 12:42:36 +0530

    Implementation of stack using Array

--- Stack Using Array/stack_using_array.c
+++ Stack Using Array/stack_using_array.c
@@ -6,6 +6,7 @@ struct stack
     int top;
     int *arr;
 };
+//Fucntion to check stack is empty.
 int isEmpty(struct stack *ptr)
 {
     if (ptr->top == -1)
@@ -14,7 +15,7 @@ int isEmpty(struct stack *ptr)
     }
     return 0; // false
 }
-
+//Function to check to stack is full.
 int isFull(struct stack *ptr)
 {
     if (ptr->top == ptr->size - 1)
@@ -26,13 +27,35 @@ int isFull(struct stack *ptr)
 
 int main()
 {
+    //Implementation without pointer
+    // struct stack s;
+    // s.size=80;
+    // s.top=-1;
+    // s.arr=(int*) malloc(sizeof(struct stack));
+    //Implementation by pointer to struct
     // Here, the stack s is a pointer to struct stack.The struct itself is allocated in heap memory.This makes the stack dynamic and can be easily passed to functions without copying the entire struct.
     struct stack *s;
     s = (struct stack *)malloc(sizeof(struct stack));
-    s->size = 80;
+    s->size = 6;
     s->top = -1;
     s->arr = (int *)malloc(s->size * sizeof(int));
-    if (isFull(s))
+
+    //Manully pushing
+    s->arr[0]=7;
+    s->top++;
+    s->arr[1]=17;
+    s->top++;
+    s->arr[2]=27;
+    s->top++;
+    s->arr[3]=47;
+    s->top++;
+    s->arr[4]=57;
+    s->top++;
+    s->arr[5]=67;
+    s->top++;
+
+    //Chcek isf stack is full
+    if (isFull(s))//Here, s itself is already a pointer to a struct.So it does not need &s.
     {
         printf("Stack is full.\n");
     }
